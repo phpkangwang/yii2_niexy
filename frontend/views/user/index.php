afc7431242037adca6289274a7f71d318be97bd2
romi <chai_romi@163.com>
2016-08-24 16:59:48 +0800

    页面

--- frontend/views/user/index.php
+++ frontend/views/user/index.php
@@ -1,49 +1,105 @@
-<?php
-   $this->title = "个人中心"; 
-?>
-<div class="userinfo-top">
-    
-   <div class="userinfo-head"><img src="images/1.jpg" /></div>
-   
-   <div class="userinfo-mobile" onclick="alert(22)"> 绑定手机号</div>
-    
-</div>
-<div class="userinfo-ordermenu userinfo-ordermenu1">
-    
-    
-     <div class="orderMsg">
-         <a class="myorder">我的订单</a>
-     </div>
-    
-</div>
-<div class="userinfo-ordermenu userinfo-ordermenu2">
-     <div class="orderMsg">
-         <a class="myorder">我的订单</a>
-     </div>
-     <div class="orderMsg">
-         <a class="myorder">我的订单</a>
-     </div>
-    
-</div>
-<div class="userinfo-ordermenu userinfo-ordermenu3">
-     <div class="orderMsg">
-         <a class="myorder">我的订单</a>
-     </div>
-     <div class="orderMsg">
-         <a class="myorder">我的订单</a>
-     </div>
-     <div class="orderMsg">
-         <a class="myorder">我的订单</a>
-     </div>
-   
+
+
+<!--
+ *@无密码快捷登陆
+-->
+<div class="frame">
+  <div class="input-frame">
+    <div class="input-tel">
+      <div class="input-qh">+86 <span class="">V</span></div>
+      <div class="input-txt"><p>18049929305</p></div>
+      <a class="input-a">获取验证码</a>
+    </div>
+    <div class="input-yzm">
+      <div class="input-yzm-t">验证码</div>
+      <div class="input-yzm-i"><input type="text" placeholder="请输入验证码" name="username"/></div>
+    </div>
+  </div>
+  <div class="box">
+    <a class="login-a up3">登陆</a>
+  </div>
 </div>
 
 
 
-<script>
- $(function(){
-       var height=$(window).height();
-       $(".userinfo-top").height(height/8);
-  });
-</script>
 
+
+
+
+
+<!--
+ *@微信
+-->
+<div class="frame">
+  <div class="weixin-frame">
+    <div class="weixin-toux">
+        <div class="toux"><img src=""/></div>
+        <div class="toux-name">
+          <p class="name-h">王康</p>
+          <p>微信号：w472366246454561</p>
+        </div>
+        <div class="arrow"><div class="arrow-down"></div></div>
+    </div>
+  </div>
+  <div class="clear-up3"></div>
+  <div class="weixin-list">
+      <div class="list"><img src=""/></div>
+      <div class="list-name">
+        <p class="name-h">相册</p>
+      </div>
+      <div class="list-arrow"><div class="arrow-down"></div></div>
+  </div>
+  <div class="weixin-list2">
+      <div class="list"><img src=""/></div>
+      <div class="list-name">
+        <p class="name-h">收藏</p>
+      </div>
+      <div class="list-arrow"><div class="arrow-down"></div></div>
+  </div>
+  <div class="weixin-list">
+      <div class="list"><img src=""/></div>
+      <div class="list-name">
+        <p class="name-h">钱包</p>
+      </div>
+      <div class="list-arrow"><div class="arrow-down"></div></div>
+  </div>
+</div>
+
+<div class="clear-up3"></div>
+<div class="weixin-list">
+    <div class="list"><img src=""/></div>
+    <div class="list-name">
+      <p class="name-h">表情</p>
+    </div>
+    <div class="list-arrow"><div class="arrow-down"></div></div>
+</div>
+
+
+
+
+
+
+
+
+
+<!--
+ *@购物车
+-->
+<div class="frame">
+  <div class="sale-frame">
+    <div class="sale-titel">
+      <div class="input-qh">+86 <span class="">V</span></div>
+      <div class="input-txt"><p>18049929305</p></div>
+      <a class="input-a">获取验证码</a>
+    </div>
+    <div class="sale-toux">
+        <div class="sale-img"><img src=""/></div>
+        <div class="sale-name">
+          <p class="sale-name-h">正品包邮|德国LAMY凌美T10</p>
+          <p class="sale-name-fl">颜色分类：天蓝（5支装）</p>
+        </div>
+    </div>
+
+
+  </div>
+</div>
